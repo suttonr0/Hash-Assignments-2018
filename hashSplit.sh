394211411adb23c670ac0aa389593206ac02a873
Rowan <sutton.r11@gmail.com>
2018-10-06 11:58:27 +0100

    First commit

---
+++ hashSplit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+grep '\$argon2i' suttonr.hashes > argonHashes.hashes
+grep '\$1\$' suttonr.hashes > md5Hashes.hashes
+grep '\$5\$' suttonr.hashes > sha256Hashes.hashes
+grep '\$6\$' suttonr.hashes > sha512Hashes.hashes
+grep '\$pbkdf2-sha256' suttonr.hashes > pbkdfHashes.hashes
+grep -v '\$argon2i|\$1\$|\$5\$|\$6\$|\$pbkdf2-sha256' suttonr.hashes > desHashes.hashes
+
