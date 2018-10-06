394ae243f01dc1ebe06d881d049e3d56ae8955af
Rowan <sutton.r11@gmail.com>
2018-10-06 12:08:20 +0100

    Added sorted hash files and fixed DEScrypt detection

--- practical4/hashSplit.sh
+++ practical4/hashSplit.sh
@@ -4,5 +4,5 @@ grep '\$1\$' suttonr.hashes > md5Hashes.hashes
 grep '\$5\$' suttonr.hashes > sha256Hashes.hashes
 grep '\$6\$' suttonr.hashes > sha512Hashes.hashes
 grep '\$pbkdf2-sha256' suttonr.hashes > pbkdfHashes.hashes
-grep -v '\$argon2i|\$1\$|\$5\$|\$6\$|\$pbkdf2-sha256' suttonr.hashes > desHashes.hashes
+grep -v -E '\$argon2i|\$1\$|\$5\$|\$6\$|\$pbkdf2-sha256' suttonr.hashes > desHashes.hashes
 
